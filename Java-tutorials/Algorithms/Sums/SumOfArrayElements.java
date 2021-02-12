809e7a62e67373afefbfdaf3ec56ae99c85ab18c
Manes Hi <manes.hipocrates@gmail.com>
2021-02-12 21:28:28 +0100

    created several folders for tutoring Java and Python

---
+++ Java-tutorials/Algorithms/Sums/SumOfArrayElements.java
@@ -0,0 +1,12 @@
+class SumOfArrayElements{
+
+public static void main(String args []){
+System.out.println("Welcome to Java tutorials");
+SumOfArrayElements sumOfArrayElements = new SumOfArrayElements();
+System.out.println(sumOfArrayElements.greetPerson(4));
+}
+
+public int greetPerson(int age){
+return age + 2;
+}
+}
