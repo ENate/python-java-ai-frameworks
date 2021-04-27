66d6fd9bbb772976913d9c3dc1ab0583d1481c7b
Manes Hi <manes.hipocrates@gmail.com>
2021-04-27 10:54:49 +0200

    Added Java 1 topics in spring, etc

---
+++ Java-tutorials/many-tutorial-topics/src/main/java/com/minejava/manytutorialtopics/ManyTutorialTopicsApplication.java
@@ -0,0 +1,13 @@
+package com.minejava.manytutorialtopics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManyTutorialTopicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManyTutorialTopicsApplication.class, args);
+	}
+
+}
