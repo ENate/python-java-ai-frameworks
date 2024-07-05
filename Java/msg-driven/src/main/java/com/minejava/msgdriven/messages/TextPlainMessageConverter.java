afd068f0960c1ef47dbbde77d020606b1418076f
ENate <nathaniel@aims.ac.za>
2024-07-05 11:01:22 +0200

    Updated to latest boot

--- Java/msg-driven/src/main/java/com/minejava/msgdriven/messages/TextPlainMessageConverter.java
+++ Java/msg-driven/src/main/java/com/minejava/msgdriven/messages/TextPlainMessageConverter.java
@@ -1,28 +1,10 @@
 package com.minejava.msgdriven.messages;
 
-import org.springframework.messaging.converter.AbstractMessageConverter;
-import org.springframework.util.MimeType;
+// import org.springframework.messaging.converter.AbstractMessageConverter;
 
-import com.minejava.msgdriven.model.LogMessage;
 
-public class TextPlainMessageConverter extends AbstractMessageConverter {
+public class TextPlainMessageConverter  {
 
-    public TextPlainMessageConverter() {
-        super(new MimeType("text", "plain"));
-    }
-
-    @Override
-    protected boolean supports(Class<?> clazz) {
-        // TO DO Auto-generated method stub
-        return (LogMessage.class == clazz);
-    }
-
-    // @Override
-    // protected Object convertFromInternal(Message<?> message, Class<?> targetClass, Object conversionHint) {
-    //     Object payload = message.getPayload();
-    //     String text = payload instanceof String ? (String) payload : new String((byte[]) payload);
-    //     return new LogMessage(text);
-    // }
 
 
 }
